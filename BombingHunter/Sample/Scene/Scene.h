106b066d034cb938cce210e761485fcf50f31abc
GG葵 <138738983+OoshiroAoi0112@users.noreply.github.com>
2024-04-12 12:11:06 +0900

    書き込み
    AppMain以外のcpp,hを書き込みました

---
+++ BombingHunter/Sample/Scene/Scene.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include "../Objects/GameObject.h"
+//#include "../Utility/Vector2D.h"
+
+class Scene
+{
+private:
+	std::vector<GameObject*>abjects;
+
+public:
+	Scene();
+	~Scene();
+
+	void Initialize();
+	void Update();
+	void Draw()const;
+	void Finalize();
+
+private:
+	//オブジェクト生成処理
+	template<class T>
+	T* CreateObject(const Vector2D& location)
+	{
+		//指定したクラスを生成する
+		t* new_instance = new T();
+
+		//GameObjectクラスを継承しているか確認
+		GameObject* new_object = dynamic_cast<GameObject*>(new_instance);
+
+		//エラーチェック
+		if (new_object == nullptr)
+		{
+			delete new_instance;
+			throw std::string("ゲームオブジェクトが生成できませんでした");
+		}
+
+		//初期化処理
+		new_object->Initialize();
+		//位置情報の設定
+		new_object0 > SetLocation(location);
+
+		//オブジェクトリストに追加
+		objects.push_back(new_object);
+
+		//インスタンスのポインタを返却
+		return new_instance;
+	}
+};
+
